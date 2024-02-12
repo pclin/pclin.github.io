e1fb8028e9305455c763237c353783aa0051b786
Po-Chiang Lin <pochianglin@gmail.com>
2024-02-12 12:07:25 +0800

    Photo jpg to png

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -2,20 +2,20 @@ var store = [{
         "title": "Computer Vision",
         "excerpt":"3D Point Cloud Classification In recent years, with the gradual advancement of artificial intelligence algorithms and big data, various industries and a growing workforce have begun investing in artificial intelligence-related fields. Notably, 3D acquisition technology and hardware equipment have made significant strides, with the collected 3D data finding widespread applications...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/research/computer_vision/"
+        "url": "/research/computer_vision/"
       },{
         "title": "Robotics",
         "excerpt":"Bi-Directional Human-Robot Interactive Task Generation System based on Large Language Models and Behavior Trees In recent years, generative artificial intelligence and large language model technologies have continued to advance, potentially changing the way robots interact with humans, understand and interpret their environment, and execute complex tasks. Through large language models,...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/research/robotics/"
+        "url": "/research/robotics/"
       },{
         "title": "Maker and STEM Education",
         "excerpt":"Robotic System Integration for Improving Learning Outcomes in the Graduation Project Capstone Course In this work, we use the robotic system integration to improve students’ learning outcomes in the Graduation Project course that is the Capstone Course in our department. The Capstone Course provides the opportunities for undergraduate students to...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/research/stem_education/"
+        "url": "/research/stem_education/"
       },{
         "title": "Wireless and Mobile Communication Networks",
         "excerpt":"Large-Scale and High-Dimensional Cell Outage Detection and Compensation Techniques in Self-Organizing Networks The objective of this research is on the self-healing in 5G Self-Organizing Network (SON). The key concept of the self-healing is to automatically detect the failure or degradation of base stations, and to automatically compensate it to mitigate...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/research/wireless/"
+        "url": "/research/wireless/"
       }]
